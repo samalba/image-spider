1537add0c7373f7fc848d00a1628be9bac2938d0
Bryan Kaplan <bkaplan@botker.com>
2012-10-30 10:05:08 -0700

    Using more portable datastore authentication.

--- image-spider-interface/models/DataModel.py
+++ image-spider-interface/models/DataModel.py
@@ -1,26 +1,24 @@
 # -*- coding: ascii -*-
 
-import json
+import os
 import postgresql
 import redis
 
 class DataModel:
 
     def __init__(self):
-        conf = json.loads(open('../environment.json').read())
-
         pq = 'pq://{0}:{1}@{2}:{3}/image_spider'
-        pq = pq.format(conf['DOTCLOUD_POSTGRES_SQL_LOGIN'],
-                       conf['DOTCLOUD_POSTGRES_SQL_PASSWORD'],
-                       conf['DOTCLOUD_POSTGRES_SQL_HOST'],
-                       conf['DOTCLOUD_POSTGRES_SQL_PORT'])
+        pq = pq.format(os.environ['DOTCLOUD_POSTGRES_SQL_LOGIN'],
+                       os.environ['DOTCLOUD_POSTGRES_SQL_PASSWORD'],
+                       os.environ['DOTCLOUD_POSTGRES_SQL_HOST'],
+                       os.environ['DOTCLOUD_POSTGRES_SQL_PORT'])
 
-        redis_credentials = {'host': conf['DOTCLOUD_REDIS_REDIS_HOST'],
-                             'port': int(conf['DOTCLOUD_REDIS_REDIS_PORT']),
-                             'password': conf['DOTCLOUD_REDIS_REDIS_PASSWORD'],
-                             'db': 0}
+        redis_cred = {'host': os.environ['DOTCLOUD_REDIS_REDIS_HOST'],
+                      'port': int(os.environ['DOTCLOUD_REDIS_REDIS_PORT']),
+                      'password': os.environ['DOTCLOUD_REDIS_REDIS_PASSWORD'],
+                      'db': 0}
 
         self.pg = postgresql.open(pq)
-        self.redis = redis.StrictRedis(**redis_credentials)
-        pubsub_redis = redis.StrictRedis(**redis_credentials)
+        self.redis = redis.StrictRedis(**redis_cred)
+        pubsub_redis = redis.StrictRedis(**redis_cred)
         self.pubsub = pubsub_redis.pubsub()
--- image-spider/data.py
+++ image-spider/data.py
@@ -7,25 +7,23 @@ class _Storefront:
     """
 
     def __init__(self):
+        import os
         import postgresql
-        import redis
-
-        conf = json.loads(open('../environment.json').read())
 
         pq = 'pq://{0}:{1}@{2}:{3}/image_spider'
-        pq = pq.format(conf['DOTCLOUD_POSTGRES_SQL_LOGIN'],
-                       conf['DOTCLOUD_POSTGRES_SQL_PASSWORD'],
-                       conf['DOTCLOUD_POSTGRES_SQL_HOST'],
-                       conf['DOTCLOUD_POSTGRES_SQL_PORT'])
+        pq = pq.format(os.environ['DOTCLOUD_POSTGRES_SQL_LOGIN'],
+                       os.environ['DOTCLOUD_POSTGRES_SQL_PASSWORD'],
+                       os.environ['DOTCLOUD_POSTGRES_SQL_HOST'],
+                       os.environ['DOTCLOUD_POSTGRES_SQL_PORT'])
 
-        redis_credentials = {'host': conf['DOTCLOUD_REDIS_REDIS_HOST'],
-                             'port': int(conf['DOTCLOUD_REDIS_REDIS_PORT']),
-                             'password': conf['DOTCLOUD_REDIS_REDIS_PASSWORD'],
-                             'db': 0}
+        redis_cred = {'host': os.environ['DOTCLOUD_REDIS_REDIS_HOST'],
+                      'port': int(os.environ['DOTCLOUD_REDIS_REDIS_PORT']),
+                      'password': os.environ['DOTCLOUD_REDIS_REDIS_PASSWORD'],
+                      'db': 0}
 
         self.pg = postgresql.open(pq)
-        self.redis = redis.StrictRedis(**redis_credentials)
-        pubsub = redis.StrictRedis(**redis_credentials)
+        self.redis = redis.StrictRedis(**redis_cred)
+        pubsub = redis.StrictRedis(**redis_cred)
         self.pubsub = pubsub.pubsub()
 
         setattr(self, 'add_webpages',
