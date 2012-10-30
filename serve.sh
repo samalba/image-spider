825d54fd8c0f2a79b3ab3926d97133d2fb39870c
Bryan Kaplan <bkaplan@botker.com>
2012-10-30 11:21:16 -0700

    Including local environmental variables for authentication to mimic the dotCloud way.

--- serve.sh
+++ serve.sh
@@ -2,9 +2,17 @@
 
 app_file=/srv/http/image-spider-interface/wsgi.py
 
+export DOTCLOUD_POSTGRES_SQL_LOGIN='image_spider'
+export DOTCLOUD_POSTGRES_SQL_PASSWORD='p3nV7qE0bbHaC8n8i'
+export DOTCLOUD_POSTGRES_SQL_HOST='localhost'
+export DOTCLOUD_POSTGRES_SQL_PORT=5432
+export DOTCLOUD_REDIS_REDIS_HOST='localhost'
+export DOTCLOUD_REDIS_REDIS_PORT=6379
+export DOTCLOUD_REDIS_REDIS_PASSWORD=
+
 sudo systemctl start nginx.service
 
-sudo -u http uwsgi \
+sudo -Eu http uwsgi \
     --socket /tmp/uwsgi.sock \
     --file $app_file \
     --master \
