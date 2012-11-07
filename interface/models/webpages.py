eb7f9575fe7030939acace740b567c161ddd248f
Bryan Kaplan <bkaplan@botker.com>
2012-11-07 15:17:33 -0800

    Adding docstrings for webpages model.

--- interface/models/webpages.py
+++ interface/models/webpages.py
@@ -5,6 +5,19 @@ from . DataModel import DataModel
 class webpages(DataModel):
 
     def add(self, child_urls, parent_url=None, depth=2):
+
+        """
+        Add a webpage to be crawled.
+
+        Arguments:
+            child_urls: list of strings URLs.
+            parent_url: optional string URL.
+            depth: optional integer depth to crawl before stopping. Level 0 is
+                   not crawled.
+
+        Returns: None.
+        """
+
         if parent_url:
             add_webpages = self.pg.proc('add_webpages(text,text[],integer)')
             add_webpages(parent_url, child_urls, depth)
@@ -14,27 +27,95 @@ class webpages(DataModel):
 
 
     def get_status(self, url):
+
+        """
+        Get the crawl status of a given URL.
+
+        Arguments:
+            url: string URL.
+
+        Returns: string status value.
+        """
+
         return self.redis.get(url) or 'ready'
 
 
     def set_status(self, url, status):
+
+        """
+        Set the crawl status of a given URL.
+
+        Arguments:
+            url: string URL.
+            status: string crawl status.
+
+        Returns: Redis success.
+        """
+
         return self.redis.set(url, status)
 
 
     def get_webpage_info(self, url):
+
+        """
+        Get metadata about a webpage.
+
+        Arguments:
+            url: string URL.
+
+        Returns: dict result of get_webpage_info stored function.
+        """
+
         get_webpage_info = self.pg.proc('get_webpage_info(text)')
         return get_webpage_info(url)
 
 
     def register_job(self, job_id, urls):
+
+        """
+        Register a new job for specified URLs.
+
+        Arguments:
+            job_id: integer Job ID.
+            urls: list of string URLs.
+
+        Returns: None.
+        """
+
         if urls:
             self.redis.sadd('job' + str(job_id), *urls) # This gets appened.
             self.redis.sadd('job' + str(job_id) + ':init', *urls)
             for url in urls:
                 self.redis.rpush('reg:' + url, job_id)
 
+
     def get_job_ids(self, url):
+
+        """
+        Retrieve the IDs of any jobs that crawled to a specified URL.
+
+        Arguments:
+            url: string URL.
+
+        Returns: list of integer Job IDs.
+        """
+
         key = 'reg:' + url
         length = self.redis.llen(key)
         byte_list = self.redis.lrange(key, 0, length)
         return [int(i) for i in byte_list]
+
+
+    def delete(self, url):
+
+        """
+        Delete a crawled webpage, any associated images, and all descendant
+        children and their associated images from the datastores.
+
+        Arguments:
+            url: string URL.
+
+        Returns: boolean success value.
+        """
+
+        return False#TODO
